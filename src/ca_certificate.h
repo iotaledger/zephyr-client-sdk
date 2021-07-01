e0059ad2dd1d00ff8d59ec5f04a0c43b7b6eaf83
Sam Chen <sam.chen@iota.org>
2021-07-01 12:51:32 +0800

    Implemented simple http client.

---
+++ src/ca_certificate.h
@@ -0,0 +1,9 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#define CA_CERTIFICATE_TAG 1
+
+// amazon root CA1
+static const unsigned char ca_certificate[] = {
+#include "app-cert.der.inc"
+};
--- src/main.c
+++ src/main.c
@@ -6,11 +6,19 @@
 
 #if defined(CONFIG_APP_WIFI_AUTO)
 #include "wifi.h"
+#elif defined(CONFIG_NET_DHCPV4)
+#include <net/net_if.h>
 #endif
 
 void main(void) {
   printk("Hello %s\n", CONFIG_BOARD);
 #if defined(CONFIG_APP_WIFI_AUTO)
+  // wifi auto connect
   wifi_connect();
+#elif defined(CONFIG_NET_DHCPV4)
+  // enable dhcpv4 for eth
+  struct net_if *iface;
+  iface = net_if_get_default();
+  net_dhcpv4_start(iface);
 #endif
 }
