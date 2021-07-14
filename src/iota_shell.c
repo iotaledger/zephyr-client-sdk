c6ea624cdea95cdc1d066c36a5999cc295364d11
Sam Chen <sam.chen@iota.org>
2021-07-14 23:36:03 +0800

    Added addr and balance commands.

--- src/iota_shell.c
+++ src/iota_shell.c
@@ -69,19 +69,72 @@ static int cmd_data(const struct shell *shell, size_t argc, char **argv) {
     if (res.is_error) {
       LOG_ERR("Err response: %s", res.u.error->msg);
       res_err_free(res.u.error);
+    } else {
+      LOG_ERR("send_indexation_msg failed (%d)", err);
     }
-    printf("send indexation failed\n");
   } else {
     printf("message ID: %s\n", res.u.msg_id);
   }
   return 0;
 }
 
+static void dump_address(iota_wallet_t *w, uint32_t index) {
+  byte_t addr_wit_version[IOTA_ADDRESS_BYTES] = {};
+  char tmp_bech32_addr[100] = {};
+
+  addr_wit_version[0] = ADDRESS_VER_ED25519;
+  wallet_address_by_index(w, index, addr_wit_version + 1);
+  address_2_bech32(addr_wit_version, w->bech32HRP, tmp_bech32_addr);
+  printf("Addr[%" PRIu32 "]\n", index);
+  // print ed25519 address without version filed.
+  printf("\t");
+  dump_hex_str(addr_wit_version + 1, ED25519_ADDRESS_BYTES);
+  // print out
+  printf("\t%s\n", tmp_bech32_addr);
+}
+
+static int cmd_addr(const struct shell *shell, size_t argc, char **argv) {
+  long s = atol(argv[1]);  // starting index
+  long n = atol(argv[2]);  // number of addresses
+  if (s < 0 || n < 0) {
+    shell_print(shell, "invalid index or numbers");
+    return -1;
+  }
+
+  for (uint32_t idx = s; idx < n; idx++) {
+    dump_address(w_ctx, idx);
+  }
+  return 0;
+}
+
+static int cmd_balance(const struct shell *shell, size_t argc, char **argv) {
+  long s = atol(argv[1]);  // starting index
+  long n = atol(argv[2]);  // number of addresses
+  if (s < 0 || n < 0) {
+    shell_print(shell, "invalid index or numbers");
+    return -1;
+  }
+
+  uint64_t value = 0;
+  for (uint32_t idx = s; idx < n; idx++) {
+    if (wallet_balance_by_index(w_ctx, idx, &value) != 0) {
+      LOG_ERR("wallet get balance [%" PRIu32 "]failed\n", idx);
+      break;
+    }
+    dump_address(w_ctx, idx);
+    printf("balance: %" PRIu64 "\n", value);
+  }
+
+  return 0;
+}
+
 /* Creating subcommands (level 1 command) array for command "iota". */
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_iota, SHELL_CMD(info, NULL, "Display node info", cmd_info),
-                               SHELL_CMD_ARG(data, NULL, "Send data message.\n Ex: iota data \"my ID\" \"hello iota\"",
-                                             cmd_data, 3, 0),
-                               SHELL_SUBCMD_SET_END);
+SHELL_STATIC_SUBCMD_SET_CREATE(
+    sub_iota, SHELL_CMD(info, NULL, "Display node info", cmd_info),
+    SHELL_CMD_ARG(data, NULL, "Send data message.\n Ex: iota data \"my ID\" \"hello iota\"", cmd_data, 3, 0),
+    SHELL_CMD_ARG(addr, NULL, "Get addresses from a given range.\n Ex: iota addr 0 5", cmd_addr, 3, 0),
+    SHELL_CMD_ARG(balance, NULL, "Get balance from a given range.\n Ex: iota balance 0 5", cmd_balance, 3, 0),
+    SHELL_SUBCMD_SET_END);
 /* Creating root (level 0) command "iota" without a handler */
 SHELL_CMD_REGISTER(iota, &sub_iota, "IOTA client demo commands", NULL);
 
@@ -119,7 +172,7 @@ static int init_http_client_conf(iota_wallet_t *w) {
     return -1;
   }
   w->endpoint.port = CONFIG_IOTA_NODE_PORT;
-  printf("Node: %s%s:%d\n", w->endpoint.use_tls ? tls_prefix : http_prefix, w->endpoint.host, w->endpoint.port);
+  LOG_INF("Node: %s%s:%d", w->endpoint.use_tls ? tls_prefix : http_prefix, w->endpoint.host, w->endpoint.port);
   return 0;
 }
 
